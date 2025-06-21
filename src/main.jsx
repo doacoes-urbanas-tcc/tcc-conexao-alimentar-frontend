d32bf51ab28c0d2fce25f2b29bd4bc5fd42b302d
yasminmartimiano <yasminliver52@gmail.com>
2025-06-21 18:24:02 -0300

    feat: configurou estrutura inicial do projeto com react

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+/*
+- service: caso precise fazer uma chamada API ou um serviço global.
+- containers: páginas
+- components: componentes gerais (ex.: button que é exibido em todas as páginas)
+*/
