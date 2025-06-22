1973ab2e5e1c02135dd56a8f11863557f2fceb8e
yasminmartimiano <yasminliver52@gmail.com>
2025-06-22 17:54:37 -0300

    build: adicionando tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold text-blue-600">Hello world!</h1>
+      <p className="mt-4 text-lg text-gray-700">
+        Isso é um teste.
+      </p>
+    </div>
+  );
+}
+
+export default App;
